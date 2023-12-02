3839350fe2d8bc1f25aa340a02ccae361bbf3fa8
niminonline <niminonline@gmail.com>
2023-12-02 12:24:41 +0530

    cors updated

--- salesyncer-server/api-gateway/src/app.ts
+++ salesyncer-server/api-gateway/src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { config } from "dotenv";
 import router from "./interfaces/routers/router";
-import cors from "cors"
+import cors from "cors";
 import logger from "./services/winston";
 
 const port = process.env.PORT || 3000;
@@ -9,29 +9,38 @@ const port = process.env.PORT || 3000;
 const app = express();
 config();
 
+const allowedOrigins = [
+  "http://localhost:4200",
+  "localhost:4200",
+  "http://localhost:80",
+  "localhost:80",
+  "http://localhost",
+  "localhost",
+  "salesyncer.cloud",
+  "http://salesyncer.cloud",
+  "https://salesyncer.cloud",
+  "http://salesyncer.cloud:80",
+  "https://salesyncer.cloud:80",
 
-const allowedOrigins = ['http://localhost:4200', 'localhost:4200','http://localhost:80','localhost:80','http://localhost','localhost'];
-
+];
 
 const corsOptions = {
-  origin: function (origin:any, callback:any) {
+  origin: function (origin: any, callback: any) {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
   },
-  credentials: true, 
-  methods: 'GET, POST, PUT, PATCH, DELETE', 
+  credentials: true,
+  methods: "GET, POST, PUT, PATCH, DELETE",
 };
-
 
 app.use(cors(corsOptions));
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/",router);
+app.use("/", router);
 
 app.listen(port, () => {
   logger.info(`API gateway listening at port# ${port}`);
