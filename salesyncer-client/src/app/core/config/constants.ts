b130f2878071841d068deb07476351d2342ac9c7
niminonline <niminonline@gmail.com>
2023-12-03 08:42:41 +0530

    changed to local api

--- salesyncer-client/src/app/core/config/constants.ts
+++ salesyncer-client/src/app/core/config/constants.ts
@@ -1,5 +1,5 @@
 
-// export const baseUrl = 'http://localhost:3000';
-export const baseUrl = 'http://api.salesyncer.cloud:3000';
+export const baseUrl = 'http://localhost:3000';
+// export const baseUrl = 'http://api.salesyncer.cloud:3000';
 
 
